708373292700a1531c8fa224623b9ef7dfb755fd
Jay Bazuzi <jbazuzi@tableau.com>
2019-08-11 14:51:26 -0700

    Jellies

--- PipelinesExercise/Exercise1/SimpleCalls.cs
+++ PipelinesExercise/Exercise1/SimpleCalls.cs
@@ -1,12 +1,29 @@
 ﻿using PipelinesExercise;
+using Refactoring.Pipelines;
+using Refactoring.Pipelines.Approvals;
 
 public class SimpleCalls
 {
-    public static Sandwich FindBestSandwich(ZipCode zipCode)
-    {
-        var peanutButters = PeanutButterShop.GetAvailable(zipCode);
-        var jellies = JellyShop.GetAvailable(zipCode);
-        var bestSandwich = Sandwich.Create(peanutButters.BestPeanutButter, jellies.BestJelly);
-        return bestSandwich;
-    }
+  public static Sandwich FindBestSandwich(ZipCode zipCode)
+  {
+    // Set up Pipeline
+    var zipCodePipe = new InputPipe<ZipCode>("zipCode");
+    var peanutButtersPipe = zipCodePipe.ProcessFunction(PeanutButterShop.GetAvailable);
+    var peanutButtersCollector = peanutButtersPipe.Collect();
+    var jelliesPipe = zipCodePipe.ProcessFunction(JellyShop.GetAvailable);
+    var jelliesCollector = jelliesPipe.Collect();
+
+    // ApprovalPipeline
+    PipelineApprovals.Verify(zipCodePipe);
+
+    // Send thru pipeline
+    zipCodePipe.Send(zipCode);
+
+    // Original code
+
+    var peanutButters = peanutButtersCollector.SingleResult;
+    var jellies = jelliesCollector.SingleResult;
+    var bestSandwich = Sandwich.Create(peanutButters.BestPeanutButter, jellies.BestJelly);
+    return bestSandwich;
+  }
 }
